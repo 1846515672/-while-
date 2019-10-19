4c55d5f25f51958da0febeab6c02910b93696398
徐金林 <55836695+1846515672@users.noreply.github.com>
2019-10-19 16:21:28 +0800

    Update 用while打印质数优化方法.py

--- 用while打印质数优化方法.py
+++ 用while打印质数优化方法.py
@@ -1,7 +1,7 @@
 from time import *
 
 class ZhiSshu:
-
+#一.
     def func1(self):
         t1 = time()
         i = 2
@@ -17,8 +17,7 @@ class ZhiSshu:
             i += 1
         t2 = time()
         print('耗时:',t2-t1,'秒')
-
-
+#二.
     def func2(self):
         t3 = time()
         i = 2
@@ -28,7 +27,7 @@ class ZhiSshu:
             while j < i:
                 if i % j == 0:
                     flag = False
-                    break
+                    break#给代码一添加break让程序执行出现不满足条件的时候不执行以下代码
                 j += 1
             if flag:
                 print(i)
@@ -36,14 +35,14 @@ class ZhiSshu:
         t4 = time()
         print('耗时:',t4-t3,'秒')
 
-
+#三.
     def func3(self):
         t5 = time()
         i = 2
         while i < 10000:
             flag = True
             j = 2
-            while j <= i ** 0.5:
+            while j <= i ** 0.5:#这里必须是<=符号,要不然打印结果不准确,会打印出4这样的数
                 if i % j == 0:
                     flag = False
                 j += 1
