46656050a420c436982a2c483491214a8f3b5e04
徐金林 <55836695+1846515672@users.noreply.github.com>
2019-10-24 15:20:16 +0800

    Update 用while打印质数优化方法.py

--- 用while打印质数优化方法.py
+++ 用while打印质数优化方法.py
@@ -1,59 +1,58 @@
 from time import *
 
-class ZhiSshu:
-#一.
-    def func1(self):
+def Number(fun):
+    def func():
         t1 = time()
-        i = 2
-        while i < 10000:
-            flag = True
-            j = 2
-            while j < i:
-                if i % j == 0:
-                    flag = False
-                j += 1
-            if flag:
-                print(i)
-            i += 1
+        fun()
         t2 = time()
         print('耗时:',t2-t1,'秒')
+    return func
+
+#一.
+@Number
+def func1():
+    i = 2
+    while i < 10000:
+        flag = True
+        j = 2
+        while j < i:
+            if i % j == 0:
+                flag = False
+            j += 1
+        if flag:
+            print(i)
+        i += 1
+
 #二.
-    def func2(self):
-        t3 = time()
-        i = 2
-        while i < 10000:
-            flag = True
-            j = 2
-            while j < i:
-                if i % j == 0:
-                    flag = False
-                    break#给代码一添加break让程序执行出现不满足条件的时候不执行以下代码
-                j += 1
-            if flag:
-                print(i)
-            i += 1
-        t4 = time()
-        print('耗时:',t4-t3,'秒')
-
+@Number
+def func2():
+    i = 2
+    while i < 10000:
+        flag = True
+        j = 2
+        while j < i:
+            if i % j == 0:
+                flag = False
+                break#给代码一添加break让程序执行出现不满足条件的时候不执行以下代码
+            j += 1
+        if flag:
+            print(i)
+        i += 1
 #三.
-    def func3(self):
-        t5 = time()
-        i = 2
-        while i < 10000:
-            flag = True
-            j = 2
-            while j <= i ** 0.5:#这里必须是<=符号,要不然打印结果不准确,会打印出4这样的数
-                if i % j == 0:
-                    flag = False
-                j += 1
-            if flag:
-                print(i)
+@Number
+def func3():
+    i = 2
+    while i < 10000:
+        flag = True
+        j = 2
+        while j <= i ** 0.5:#这里必须是<=符号,要不然打印结果不准确,会打印出4这样的数
+            if i % j == 0:
+                flag = False
                 break
-            i += 1
-        t6 = time()
-        print('耗时:',t6-t5,'秒')
-
-z = ZhiSshu()
-# z.func1()
-# z.func2()
-# z.func3()
+            j += 1
+        if flag:
+            print(i)
+        i += 1
+# func1()
+# func2()
+# func3()
